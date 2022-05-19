5dc2feafcc84d2aef9a6fb0bbbdf30b7106834bd
Lyle Buendia <99925031+mast3rleaf@users.noreply.github.com>
2022-05-19 04:42:45 -0700

    pseudocode

--- lab_5.py
+++ lab_5.py
@@ -33,21 +33,3 @@ create_dictionary(infile):
   return the dict
 main()
 """
-
-def main():
-  sentence = input("Enter a sentence: ")
-  dictionary = create_dictionary("textese.txt")
-  translate(sentence, dictionary)
-
-def create_dictionary(txt_file):
-  infile = open(txt_file, "r")
-  words = [word.rstrip() for word in infile]
-  infile.close()
-  return dict([word.split(",") for word in words])
-
-def translate(sentence, dictionary):
-  words = sentence.split()
-  for word in words:
-    print(dictionary.get(word, word), " ", end="")
-
-main()
